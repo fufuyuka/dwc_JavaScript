4d5c190e496c49248da0c0ae5829e6c4fc4df121
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-08-01 13:21:36 +0000

    [add]let,alert,conditional branching

---
+++ app.js
@@ -0,0 +1,36 @@
+let hello = "Hello World";
+// alert(hello);
+
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+// 文字列型があると変数の数値型が型強制される
+// alert(int1 + ' ' + int2 + ' ' + float1 + ' ' + str1);
+
+// 文字列型の結合
+let str2 = 'Hello';
+let str3 = 'World!!';
+// alert(str2 + str3);
+
+
+// 条件分岐
+let orange = 100;
+let apple = 120;
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+

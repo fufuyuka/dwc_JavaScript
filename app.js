e1116ec46757ebbf42965cab67c6c8fa9dc91afc
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-08-02 15:33:51 +0000

    [add]while,do...while

--- app.js
+++ app.js
@@ -34,3 +34,23 @@ if(orange < apple){
   alert('みかんの値段がりんごより高い');
 }
 
+// 繰り返し処理
+// While文 条件式がtureなら繰り返す
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// 【補足】do … while文
+// 条件式の真偽に関わらず最初の1回だけは必ず処理される
+// do{
+//   最低1回は行われる処理
+// }while(条件式);
+
+

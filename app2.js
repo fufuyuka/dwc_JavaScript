2cb11c95058fa599238465afd7a88b2599f04663
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-08-04 15:15:29 +0000

    [add]変数のスコープ

--- app2.js
+++ app2.js
@@ -1,1 +1,22 @@
+var str = "webcamp" //グローバルスコープ
 
+function foo() {
+  console.log(str)
+  var y = "hello" //関数スコープ
+}
+
+foo()
+console.log(y) //関数外から呼び出せずエラーになる
+
+
+
+function foo() {
+  let x = "webcamp"
+  {
+    let y = "hello webcamp"
+  }
+  console.log(x)
+  console.log(y)
+}
+
+foo()
